c683132a2a38a76275281eb105c1b7dbcbccf7e4
JeonMinKi <jeonminki12@gmail.com>
2019-04-21 13:57:38 +0900

    1

---
+++ Winform/WinForm/CheckBox/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                checkBox1.Text = "Checked";
+            }
+            else
+            {
+                checkBox1.Text = "Unchecked";
+            }
+
+            //////////////////////////////////////////////
+            switch (checkBox1.CheckState)
+            {
+                case CheckState.Checked:
+                    // Code for checked state.  
+                    break;
+                case CheckState.Unchecked:
+                    // Code for unchecked state.  
+                    break;
+                case CheckState.Indeterminate:
+                    // Code for indeterminate state.  
+                    break;
+            }
+           
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox2.CheckState == CheckState.Checked)
+            {
+                this.AllowDrop = false;
+            }
+            
+        }
+    }
+}
---
+++ Winform/WinForm/TextBoxControl/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextBoxControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //여기로 안들어오는데?
+        private void textBox1_Enter(object sender, System.EventArgs e)
+        {
+            textBox1.SelectionStart = 0;
+            textBox1.SelectionLength = 0;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectionStart = 0;
+            textBox1.SelectionLength = 0;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
