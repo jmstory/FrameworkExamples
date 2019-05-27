5e45e3895da0c7341f6e8649f138a77978fd34b3
JeonMinKi <jeonminki12@gmail.com>
2019-05-28 06:53:22 +0900

    Synchronous Client/Server

---
+++ SocketExample/Synchronous/Client/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;  
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text;  
+
+namespace Client
+{
+    class Program
+    {
+        public static void StartClient()
+        {
+            //데이터 버퍼
+            byte[] bytes = new byte[1024];
+
+            //접속
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress,11000);
+
+                //소켓생성TCP/IP
+                Socket sender = new Socket(ipAddress.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+
+                //소켓 접속 to 리모트 엔드포인트. 에러 캐치
+                try
+                {
+                    sender.Connect(remoteEP);
+                    Console.WriteLine("socket connected to {0}",sender.RemoteEndPoint.ToString());
+
+                    //바이트 어레이에 문자열 데이터 쓰기
+                    byte[] msg = Encoding.ASCII.GetBytes("this is a test<EOF>");
+
+                    //소켓을 통해 데이터 전송
+                    int byteSent = sender.Send(msg);
+
+                    //리모트 디바이스에서 응답받기
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("echoed test = {0}",Encoding.ASCII.GetString(bytes,0,bytesRec));
+
+                    //소켓해제
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch(ArgumentException ane)
+                {
+                    Console.WriteLine("ArgumentException : {0}",ane.ToString());
+                }
+                catch(SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}",se.ToString());
+                }
+                 catch(Exception  e)
+                {
+                    Console.WriteLine("Exception  : {0}",e.ToString());
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+           StartClient();
+        }
+    }
+}
---
+++ SocketExample/Synchronous/Server/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{   
+    class Program
+    {
+        //클라이언트로 부터 오는 데이터
+        public static string data = null;
+        
+        public static void StartListening()
+        {
+            //들어오는 데이터 버퍼
+            byte[] bytes = new Byte[1024];
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress , 11000);
+
+            //소켓생성TCP/IP
+            Socket listener = new Socket(ipAddress.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+
+            //바인드소켓
+            //접속시도 리슨
+            try{
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                //리스닝 시작
+                while(true)
+                {
+                    Console.WriteLine("waiting for a connection");
+
+                    //접속시도가 있을때까지 프로그램은 정지
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    //접속시도시 데이터 진행필요
+                    while(true)
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes,0,bytesRec);
+                        if(data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    //콘솔창에 데이터 보여주기
+                    Console.WriteLine("Text Recvied : {0}",data);
+
+                    //데이터 클라이언트로 보내기
+                    byte[] msg =Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nPress Enter to continue");
+            Console.Read();
+            
+
+        }
+        static void Main(string[] args)
+        {
+           StartListening();
+         
+        }
+    }
+}
