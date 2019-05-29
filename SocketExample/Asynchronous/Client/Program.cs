f2c134e34f06a5a5d463b6d181e0c0521896d4db
JeonMinKi <jeonminki12@gmail.com>
2019-05-29 13:49:30 +0900

    Asynchronous Client / Server 클라이언트 접속 안됨 dns호스트네임때문인듯

---
+++ SocketExample/Asynchronous/Client/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;  
+using System.Net;  
+using System.Net.Sockets;  
+using System.Threading;  
+using System.Text;  
+
+namespace Client
+{
+    public class StateObject
+    {
+        public Socket workSocket = null;
+        public const int bufferSize =256;
+        public byte [] buffer = new byte[bufferSize];
+        public StringBuilder sb = new StringBuilder();
+    }
+    class Program
+    {
+        private const int port = 11000;
+
+        private static ManualResetEvent connectDone = new ManualResetEvent(false);
+        private static ManualResetEvent sendDone = new ManualResetEvent(false);
+        private static ManualResetEvent receiveDone = new ManualResetEvent(false);
+        private static String response = String.Empty;
+
+        private static void StartClient()
+        {
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry("host.contoso.com");
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress,port);
+
+                //클라이언트 TCP/IP
+                Socket client = new Socket(ipAddress.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+
+                //remote endpoint 접속
+                client.BeginConnect(remoteEP, new AsyncCallback(ConnectCallback),client);
+                connectDone.WaitOne();
+
+                //리모트 디바이스에 데이터 보내기
+                Send(client,"This is a test<EOF>");
+                sendDone.WaitOne();
+
+                //리모트 디바이스로부터 리시브 응답
+                Receive(client);
+                receiveDone.WaitOne();
+
+                //콘솔창에 응답 보여주기
+                Console.WriteLine("Response received : {0}",response);
+
+                //소켓 해제
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = (Socket)ar.AsyncState;
+
+                client.EndConnect(ar);
+
+                Console.WriteLine("Socket connected to {0}",client.RemoteEndPoint.ToString());
+
+                connectDone.Set();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        private static void Receive(Socket client)
+        {
+            try
+            {
+                StateObject state = new StateObject();
+                state.workSocket = client;
+
+                client.BeginReceive(state.buffer,0,StateObject.bufferSize,0, new AsyncCallback(ReceiveCallback),state);                
+            }
+            catch(Exception e)
+            {
+                 Console.WriteLine(e.ToString());
+            }
+        }
+        private static void ReceiveCallback( IAsyncResult ar)
+        {
+            try
+            {
+                StateObject state =(StateObject)ar.AsyncState;
+                Socket client = state.workSocket;
+
+                int bytesRead = client.EndReceive(ar);
+
+                if(bytesRead > 0)
+                {
+                    state.sb.Append(Encoding.ASCII.GetString(state.buffer,0,bytesRead));
+                    client.BeginReceive(state.buffer,0,StateObject.bufferSize,0, new AsyncCallback(ReceiveCallback),state);                
+                }
+                else
+                {
+                    if(state.sb.Length > 1)
+                    {
+                        response = state.sb.ToString();
+                    }
+                    receiveDone.Set();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void Send(Socket client, String data)
+        {
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            client.BeginSend(byteData,0,byteData.Length,0, new AsyncCallback(SendCallback),client);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = (Socket)ar.AsyncState;
+                int bytesSent = client.EndSend(ar);
+                Console.WriteLine("Sent{0} bytes to server.",bytesSent);
+                sendDone.Set();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            StartClient();
+        }
+    }
+}
---
+++ SocketExample/Asynchronous/Server/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;  
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text;  
+using System.Threading;
+
+namespace Server
+{
+    public class StateObject 
+    {         
+        public Socket workSocket = null;         
+        public const int BufferSize = 1024;  
+        public byte[] buffer = new byte[BufferSize];  
+        public StringBuilder sb = new StringBuilder();    
+    }  
+    class Program
+    {
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        public Program()
+        {
+
+        }
+
+        public static void StartListening()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress,11000);
+
+            //소켓생성 TCP/IP
+            Socket listner = new Socket(ipAddress.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+
+            //로컬엔드포인트와 리슨을 바인드
+            try
+            {
+                listner.Bind(localEndPoint);
+                listner.Listen(100);
+
+                while(true)
+                {
+                    //노시그널 스테이트 이벤트 설정
+                    allDone.Reset();
+
+                    //asynchronous 소켓에 접속시도 하는것을 시작
+                    Console.WriteLine("waiting for a connection.");
+                    listner.BeginAccept(new AsyncCallback(AcceptCallback),listner);
+                    
+                    //접속시도하는동안 대기
+                    allDone.WaitOne();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress Enter to countinue");
+            Console.Read();
+        }
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            //시그널 메인스레드
+            allDone.Set();
+
+            //클라이언트 리퀘스트 핸들 소켓을 가져온다
+            Socket listner = (Socket)ar.AsyncState;
+            Socket handler = listner.EndAccept(ar);
+
+            //스테이트 오브젝트 생성
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer,0,StateObject.BufferSize,0, new AsyncCallback(ReadCallback),state);            
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket  
+            // from the asynchronous state object.  
+            StateObject state = (StateObject) ar.AsyncState;  
+            Socket handler = state.workSocket;  
+
+            //클라이언트 소켓으로부터 데이터를 읽는다
+            int bytesRead = handler.EndReceive(ar);
+            
+            if(bytesRead > 0)
+            {
+                //데이터가 더 있을수 있기때문에 ,데이터를 저장하고 더읽어온다
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer,0,bytesRead));
+
+                //end of file 태그를 체크한다. 만약 아니라면 데이터를 더 읽어온다
+                content = state.sb.ToString();
+                if(content.IndexOf("<EOF>") > - 1)
+                {
+                    //모든 데이터를 읽었으니 콘솔창에 나타낸다
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",content.Length,content);
+                    
+                    //데이터를 클라이언트로 다시 보낸다
+                    Send(handler,content);
+                }
+                else
+                {
+                    //데이터를 다 못읽었으니 더 읽어온다
+                    handler.BeginReceive(state.buffer,0,StateObject.BufferSize,0, new AsyncCallback(ReadCallback),state);
+                }
+            }
+        }
+
+        private static void Send(Socket handler , String data)
+        {
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            handler.BeginSend(byteData , 0 , byteData.Length,0, new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket handler = (Socket)ar.AsyncState;
+                
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client",bytesSent);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            StartListening();
+        }
+    }
+}
