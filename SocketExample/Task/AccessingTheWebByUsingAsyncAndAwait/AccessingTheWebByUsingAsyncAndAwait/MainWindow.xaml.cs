745de230810c4326bb2d774598ea8195969cda7d
JeonMinKi <jeonminki12@gmail.com>
2019-06-05 12:47:50 +0900

    Task.WhenAll / AccesingTheWeb

---
+++ SocketExample/Task/AccessingTheWebByUsingAsyncAndAwait/AccessingTheWebByUsingAsyncAndAwait/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System.Net.Http;
+using System.Net;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccessingTheWebByUsingAsyncAndAwait
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //ss/tls 보안채널 생성불가
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            startButton.IsEnabled = false;
+
+            resultsTextBox.Clear();
+
+            //await SumPageSizes();
+            await SumPageSizesAsync();
+
+            resultsTextBox.Text += "\r\nControl returned to startButton_Click.";
+
+            startButton.IsEnabled = true;
+        }
+
+        private async Task SumPageSizesAsync()
+        {
+            HttpClient client =
+               new HttpClient() { MaxResponseContentBufferSize = 1000000 };
+
+            List<string> urlList = SetUpURLList();
+
+            var total = 0;
+
+            foreach (var url in urlList)
+            {
+                byte[] urlContents = await client.GetByteArrayAsync(url);
+                DisplayResults(url, urlContents);
+                total += urlContents.Length;
+            }
+            resultsTextBox.Text +=
+                $"\r\n\r\nTotal bytes returned:  {total}\r\n";
+
+        }
+        private async Task SumPageSizes()
+        {
+            // Make a list of web addresses.
+            List<string> urlList = SetUpURLList();
+
+            var total = 0;
+            foreach (var url in urlList)
+            {
+                // GetURLContents returns the contents of url as a byte array.
+                byte[] urlContents = await GetURLContents(url);
+
+                DisplayResults(url, urlContents);
+
+                // Update the total.
+                total += urlContents.Length;
+            }
+
+            // Display the total count for all of the web addresses.
+            resultsTextBox.Text += $"\r\n\r\nTotal bytes returned:  {total}\r\n";
+        }
+
+        private List<string> SetUpURLList()
+        {
+            var urls = new List<string>
+            {
+                "https://msdn.microsoft.com/library/windows/apps/br211380.aspx",
+                "https://msdn.microsoft.com",
+                "https://msdn.microsoft.com/library/hh290136.aspx",
+                "https://msdn.microsoft.com/library/ee256749.aspx",
+                "https://msdn.microsoft.com/library/hh290138.aspx",
+                "https://msdn.microsoft.com/library/hh290140.aspx",
+                "https://msdn.microsoft.com/library/dd470362.aspx",
+                "https://msdn.microsoft.com/library/aa578028.aspx",
+                "https://msdn.microsoft.com/library/ms404677.aspx",
+                "https://msdn.microsoft.com/library/ff730837.aspx"
+            };
+            return urls;
+        }
+
+        private async Task<byte[]> GetURLContents(string url)
+        {
+            // The downloaded resource ends up in the variable named content.
+            var content = new MemoryStream();
+
+            // Initialize an HttpWebRequest for the current URL.
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+
+            // Send the request to the Internet resource and wait for
+            // the response.
+            using (WebResponse response = await webReq.GetResponseAsync())
+
+            // The previous statement abbreviates the following two statements.
+
+            //Task<WebResponse> responseTask = webReq.GetResponseAsync();
+            //using (WebResponse response = await responseTask)
+            {
+                // Get the data stream that is associated with the specified url.
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    // Read the bytes in responseStream and copy them to content.
+                    await responseStream.CopyToAsync(content);
+
+                    // The previous statement abbreviates the following two statements.
+
+                    // CopyToAsync returns a Task, not a Task<T>.
+                    //Task copyTask = responseStream.CopyToAsync(content);
+
+                    // When copyTask is completed, content contains a copy of
+                    // responseStream.
+                    //await copyTask;
+                }
+            }
+            // Return the result as a byte array.
+            return content.ToArray();
+        }
+
+        private void DisplayResults(string url, byte[] content)
+        {
+            // Display the length of each website. The string format
+            // is designed to be used with a monospaced font, such as
+            // Lucida Console or Global Monospace.
+            var bytes = content.Length;
+            // Strip off the "https://".
+            var displayURL = url.Replace("https://", "");
+            resultsTextBox.Text += $"\n{displayURL,-58} {bytes,8}";
+        }
+    }
+}
---
+++ SocketExample/Task/AccessingTheWebByUsingAsyncAndAwait/TaskWhenAll/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System.Net.Http;
+using System.Net;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskWhenAll
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //ss/tls 보안채널 생성불가
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            resultsTextBox.Clear();
+
+            //Task sumTask = SumPageSizesAsync();
+            Task sumTask = SumPageSizesAsyncHttpClinet();
+            await sumTask;
+            resultsTextBox.Text += "\r\nControl returned to startButton_Click.\r\n";  
+        }
+
+        private async Task SumPageSizesAsync()
+        {
+            List<string> urlList = SetUpURLList();
+            IEnumerable<Task<int>> downloadTasksQuery = from url in urlList select ProcessURLAsync(url);
+
+            Task<int>[] downloadTasks = downloadTasksQuery.ToArray();
+            int[] lengths = await Task.WhenAll(downloadTasks);
+            int total = lengths.Sum();
+
+            //var total = 0;  
+            //foreach (var url in urlList)  
+            //{  
+            //    byte[] urlContents = await GetURLContentsAsync(url);  
+
+            //    // The previous line abbreviates the following two assignment statements.  
+            //    // GetURLContentsAsync returns a Task<T>. At completion, the task  
+            //    // produces a byte array.  
+            //    //Task<byte[]> getContentsTask = GetURLContentsAsync(url);  
+            //    //byte[] urlContents = await getContentsTask;  
+
+            //    DisplayResults(url, urlContents);  
+
+            //    // Update the total.            
+            //    total += urlContents.Length;  
+            //}  
+
+            resultsTextBox.Text +=
+                $"\r\n\r\nTotal bytes returned:  {total}\r\n";
+        }
+       
+        private List<string> SetUpURLList()
+        {
+            List<string> urls = new List<string>
+            {
+                "https://msdn.microsoft.com",
+                "https://msdn.microsoft.com/library/windows/apps/br211380.aspx",
+                "https://msdn.microsoft.com/library/hh290136.aspx",
+                "https://msdn.microsoft.com/library/ee256749.aspx",
+                "https://msdn.microsoft.com/library/hh290138.aspx",
+                "https://msdn.microsoft.com/library/hh290140.aspx",
+                "https://msdn.microsoft.com/library/dd470362.aspx",
+                "https://msdn.microsoft.com/library/aa578028.aspx",
+                "https://msdn.microsoft.com/library/ms404677.aspx",
+                "https://msdn.microsoft.com/library/ff730837.aspx"
+            };
+            return urls;
+        }
+
+        private async Task<int> ProcessURLAsync(string url)
+        {
+            var byteArray = await GetURLContentsAsync(url);
+            DisplayResults(url, byteArray);
+            return byteArray.Length;
+        }
+
+        private async Task<byte[]> GetURLContentsAsync(string url)
+        {
+            // The downloaded resource ends up in the variable named content.  
+            var content = new MemoryStream();
+
+            // Initialize an HttpWebRequest for the current URL.  
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+
+            // Send the request to the Internet resource and wait for  
+            // the response.  
+            using (WebResponse response = await webReq.GetResponseAsync())
+            {
+                // Get the data stream that is associated with the specified url.  
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    await responseStream.CopyToAsync(content);
+                }
+            }
+
+            // Return the result as a byte array.  
+            return content.ToArray();
+
+        }
+
+        private async Task SumPageSizesAsyncHttpClinet()
+        {
+            List<string> urlList = SetUpURLList();
+            HttpClient client = new HttpClient() { MaxResponseContentBufferSize = 1000000 };
+
+            IEnumerable<Task<int>> downloadTasksQuery = from url in urlList select ProcessURLAsync(url, client);
+            Task<int>[] downloadTasks = downloadTasksQuery.ToArray();
+            int[] lengths = await Task.WhenAll(downloadTasks);
+            int total = lengths.Sum();
+
+            resultsTextBox.Text += $"\r\n\r\nTotal bytes returned:  {total}\r\n";
+        }
+
+        async Task<int> ProcessURLAsync(string url, HttpClient client)
+        {
+            byte[] byteArray = await client.GetByteArrayAsync(url);
+            DisplayResults(url, byteArray);
+            return byteArray.Length;
+        }
+
+
+        private void DisplayResults(string url, byte[] content)
+        {
+            // Display the length of each website. The string format   
+            // is designed to be used with a monospaced font, such as  
+            // Lucida Console or Global Monospace.  
+            var bytes = content.Length;
+            // Strip off the "https://".  
+            var displayURL = url.Replace("https://", "");
+            resultsTextBox.Text += $"\n{displayURL,-58} {bytes,8}";
+        }
+    }
+}
