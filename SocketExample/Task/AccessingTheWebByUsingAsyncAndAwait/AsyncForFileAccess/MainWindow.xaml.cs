4583267e5eace6a9d7ae852d6d82e13331e2245f
JeonMinKi <jeonminki12@gmail.com>
2019-06-12 13:03:09 +0900

    HandlingReEntrancy / AsyncForFileAccess

---
+++ SocketExample/Task/AccessingTheWebByUsingAsyncAndAwait/AsyncForFileAccess/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Diagnostics;
+using System.IO;
+
+
+namespace AsyncForFileAccess
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async void ProcessWrite()
+        {
+            string filePath = @"temp2.txt";
+            string text = "Hello World\r\n";
+
+            await WriteTextAsync(filePath, text);
+        }
+
+        //Writing Text
+        private async Task WriteTextAsync(string filePath, string text)
+        {
+            byte[] encodedText = Encoding.Unicode.GetBytes(text);
+
+            using (FileStream sourceStream = new FileStream(filePath,
+                FileMode.Append, FileAccess.Write, FileShare.None,
+                bufferSize: 4096, useAsync: true))
+            {
+                await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+            };
+        }
+
+        //Reading Text
+        public async void ProcessRead()
+        {
+            string filePath = @"temp2.txt";
+
+            if (File.Exists(filePath) == false)
+            {
+                Debug.WriteLine("file not found: " + filePath);
+            }
+            else
+            {
+                try
+                {
+                    string text = await ReadTextAsync(filePath);
+                    Debug.WriteLine(text);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+        private async Task<string> ReadTextAsync(string filePath)
+        {
+            using (FileStream sourceStream = new FileStream(filePath,
+                FileMode.Open, FileAccess.Read, FileShare.Read,
+                bufferSize: 4096, useAsync: true))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                byte[] buffer = new byte[0x1000];
+                int numRead;
+                while ((numRead = await sourceStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    string text = Encoding.Unicode.GetString(buffer, 0, numRead);
+                    sb.Append(text);
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        //Parallel Asynchronous I/O
+        public async void ProcessWriteMult()
+        {
+            string folder = @"tempfolder\";
+            List<Task> tasks = new List<Task>();
+            List<FileStream> sourceStreams = new List<FileStream>();
+
+            try
+            {
+                for (int index = 1; index <= 10; index++)
+                {
+                    string text = "In file " + index.ToString() + "\r\n";
+
+                    string fileName = "thefile" + index.ToString("00") + ".txt";
+                    string filePath = folder + fileName;
+
+                    byte[] encodedText = Encoding.Unicode.GetBytes(text);
+
+                    FileStream sourceStream = new FileStream(filePath,
+                        FileMode.Append, FileAccess.Write, FileShare.None,
+                        bufferSize: 4096, useAsync: true);
+
+                    Task theTask = sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+                    sourceStreams.Add(sourceStream);
+
+                    tasks.Add(theTask);
+                }
+
+                await Task.WhenAll(tasks);
+            }
+
+            finally
+            {
+                foreach (FileStream sourceStream in sourceStreams)
+                {
+                    sourceStream.Close();
+                }
+            }
+        }
+    }
+}
---
+++ SocketExample/Task/AccessingTheWebByUsingAsyncAndAwait/HandlingReEntrancy/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+namespace HandlingReEntrancy
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //ss/tls 보안채널 생성불가
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await AccessTheWebAsync();
+            }
+            catch (Exception)
+            {
+                resultsTextBox.Text += "\r\nDownloads failed.";
+            }
+        }
+
+        private async Task AccessTheWebAsync()
+        {
+            // Declare an HttpClient object.
+            HttpClient client = new HttpClient();
+
+            // Make a list of web addresses.
+            List<string> urlList = SetUpURLList();
+
+            var total = 0;
+            var position = 0;
+
+            foreach (var url in urlList)
+            {
+                // GetByteArrayAsync returns a task. At completion, the task
+                // produces a byte array.
+                byte[] urlContents = await client.GetByteArrayAsync(url);
+
+                DisplayResults(url, urlContents, ++position);
+
+                // Update the total.
+                total += urlContents.Length;
+            }
+
+            // Display the total count for all of the websites.
+            resultsTextBox.Text +=
+                $"\r\n\r\nTOTAL bytes returned:  {total}\r\n";
+        }
+
+        private List<string> SetUpURLList()
+        {
+            List<string> urls = new List<string>
+            {
+                "https://msdn.microsoft.com/library/hh191443.aspx",
+                "https://msdn.microsoft.com/library/aa578028.aspx",
+                "https://msdn.microsoft.com/library/jj155761.aspx",
+                "https://msdn.microsoft.com/library/hh290140.aspx",
+                "https://msdn.microsoft.com/library/hh524395.aspx",
+                "https://msdn.microsoft.com/library/ms404677.aspx",
+                "https://msdn.microsoft.com",
+                "https://msdn.microsoft.com/library/ff730837.aspx"
+            };
+            return urls;
+        }
+
+        private void DisplayResults(string url, byte[] content, int pos)
+        {
+            // Display the length of each website. The string format is designed
+            // to be used with a monospaced font, such as Lucida Console or
+            // Global Monospace.
+
+            // Strip off the "https://".
+            var displayURL = url.Replace("https://", "");
+            // Display position in the URL list, the URL, and the number of bytes.
+            resultsTextBox.Text += $"\n{pos}. {displayURL,-58} {content.Length,8}";
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
+
